7b791d33b79fba478a312cbe92eb40abebec7d6d
nondefault <spam@nondefault.net>
2022-08-05 02:07:42 -0700

    update animation code

--- peripheral/LED.h
+++ peripheral/LED.h
@@ -1,57 +1,125 @@
 #include <FastLED.h>
 
-#define LED_COUNT 12
+#define N_LEDS 12
 #define PIN_LED 9
 
-#define MODULE_COUNT 6
+#define N_MODULES 6
 
-CRGB leds[LED_COUNT];
-double brightness[MODULE_COUNT];
-double targetBrightness[MODULE_COUNT];
+CRGB leds[N_LEDS];
 
-int pos = 0;
-unsigned long lastTime;
-unsigned long lastUpdateTime;
+CRGB hot_color(float heat, float brightness) {
+  float heat3 = heat * heat * heat;
+  return CRGB(
+    brightness * heat * 255, 
+    brightness * heat3 * 255, 
+    brightness * heat3 * heat3 * 255
+  );
+}
 
-// 0x54d9d3
+CRGB hot_color(float heat, float brightness, uint8_t hue) {
+  float heat2 = heat * heat;
+  uint8_t i1 = brightness * heat * 255;
+  uint8_t i2 = brightness * heat2 * 255;
+  uint8_t i3 = brightness * heat2 * heat2 * 255;
+  return CHSV(hue, 255, i1) + CHSV(hue + 255 / 3, 255, i2) + CHSV(hue + 255 * 2 / 3, 255, i3);
+}
+
+enum AnimationMode {TEST, WHITE, ACTIVE};
+
+struct Animation {
+  int module_index;
+  CRGB* const leds;
+  AnimationMode mode = ACTIVE;
+  
+  unsigned long clock_prev_ms = 0;
+  unsigned long clock_ms = 0;
+  float dt_s = 0;
+  
+  int stage = 0;
+  unsigned long stage_start_ms = 0;
+
+  float activation_rate = 0.5;
+  float activation_target = 0;
+  float activation = 0;
+
+  Animation(int module_index, CRGB* const leds, unsigned long clock_ms) 
+    : module_index(module_index),
+      leds(leds), 
+      clock_prev_ms(clock_ms),
+      clock_ms(clock_ms) {}
+
+  void update(unsigned long clock_ms, int stage) {
+    this->dt_s = (clock_ms - this->clock_prev_ms) / 1000.0;
+    this->clock_prev_ms = this->clock_ms;
+    this->clock_ms = clock_ms;
+    if (this->stage != stage) {
+      this->stage = stage;
+      this->stage_start_ms = clock_ms;
+    }
+
+    switch (mode) {
+      case TEST:
+        updateTest();
+        break;
+      case WHITE:
+        updateWhite();
+        break;
+      case ACTIVE:
+        updateActive();
+        break;
+    }
+  }
+
+private:
+  void updateTest() {
+    static const unsigned long d = 200;
+    if (clock_ms / d % N_MODULES == module_index) {
+      fill_solid(leds, N_LEDS, CHSV(module_index * 255 / N_MODULES, 255, 255));
+    }
+  }
+
+  void updateWhite() {
+    fill_solid(leds, N_LEDS, CRGB(255, 255, 255));
+  }
+
+  void updateActive() {
+    if (module_index < stage) {
+      // activated
+      activation_target = 1.0;
+    } else {
+      activation_target = 0.0;
+    }
+    activation += (activation_target - activation) * activation_rate * dt_s;
+
+    for (int i = 0; i < N_LEDS; ++i) {
+      float heat = inoise8(clock_ms / 4, module_index * 67 + clock_ms / 3, i * 67) / 255.0;
+      leds[i] = hot_color(heat, 0.5, 180);
+    }
+
+    for (int i = 0; i < N_LEDS; ++i) {
+      float gradient = 1.0 - abs(float(i) / (N_LEDS - 1) - 0.5) * 2.0 + 1.0;
+      float f = constrain(gradient * activation, 0.0, 1.0);
+      f *= f;
+      f *= (inoise8(clock_ms, module_index * 67, i * 67) / 255.0) * 0.3 + 0.7;
+      CRGB col = hot_color(f, 1.0);
+      leds[i] += col;
+    }
+  }
+};
+
+Animation anim{0, leds, millis()};
 
 void setupLeds() {
-  for (int i = 0; i < MODULE_COUNT; ++i) {
-    brightness[i] = 0;
-    targetBrightness[i] = 0;
-  }
-  
-  FastLED.addLeds<NEOPIXEL, PIN_LED>(leds, LED_COUNT)
+  FastLED.addLeds<NEOPIXEL, PIN_LED>(leds, N_LEDS)
     .setCorrection(CRGB(255, 150, 100));
-  lastTime = millis();
-  lastUpdateTime = micros();
 }
 
 void updateLeds() {
-  // simulation
-  if (millis() - lastTime > 2000) {
-    lastTime = millis();
-    pos = (pos + 1) % MODULE_COUNT;
-  }
-  for (int i = pos; i < MODULE_COUNT; ++i) {
-    targetBrightness[i] = 0.0;
-  }
-  targetBrightness[pos] = 1.0;
-
-  double delta = (micros() - lastUpdateTime) / 1000000.0;
-  lastUpdateTime = micros();
-
-  double animSpeed = 1.0 * delta;
-  for (int i = 0; i < MODULE_COUNT; ++i) {
-    brightness[i] = brightness[i] * (1.0 - animSpeed) + targetBrightness[i] * animSpeed;
-  }
-
-  double t = millis()/1000.0;
+  // interactivity simulation
+  int stage = millis() / 2000 % (N_MODULES + 1);
+  unsigned long clock_ms = millis();
 
   FastLED.clear();
-  for (int i = 0; i < MODULE_COUNT; ++i) {
-    double b = max(0.1, brightness[i]);
-    leds[i] = HeatColor(b * 0.75 * 255);
-  }
+  anim.update(clock_ms, stage);
   FastLED.show();
 }
